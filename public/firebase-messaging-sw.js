2807b0952d4b05135c6b355be2b9995718e3262d
etyanglinus <etyanglinus453@gmail.com>
2025-12-29 15:47:43 +0300

    configured firebase

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -6,13 +6,13 @@ importScripts(
 );
 // // Initialize the Firebase app in the service worker by passing the generated config
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+  apiKey: "AIzaSyCQpRXx3zNwG0E2GBeK3KbKpvT7brifAqY",
+  authDomain: "fasta-rides-457315.firebaseapp.com",
+  projectId: "fasta-rides-457315",
+  storageBucket: "fasta-rides-457315.firebasestorage.app",
+  messagingSenderId: "833886382257",
+  appId: "1:833886382257:web:4823e7d0334a723695da90",
+  measurementId: "G-B8TYEPE09Z"
 };
 
 firebase?.initializeApp(firebaseConfig);
--- src/firebase.js
+++ src/firebase.js
@@ -8,12 +8,13 @@ import {
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyCQpRXx3zNwG0E2GBeK3KbKpvT7brifAqY",
+  authDomain: "fasta-rides-457315.firebaseapp.com",
+  projectId: "fasta-rides-457315",
+  storageBucket: "fasta-rides-457315.firebasestorage.app",
+  messagingSenderId: "833886382257",
+  appId: "1:833886382257:web:4823e7d0334a723695da90",
+  measurementId: "G-B8TYEPE09Z"
 };
 
 const firebaseApp = !getApps().length
@@ -44,7 +45,7 @@ export const fetchToken = async (setTokenFound, setFcmToken) => {
 
     const currentToken = await getToken(messaging, {
       vapidKey:
-        "",
+        "BH9bdhUCaGYcpUs0tzLdufqXmPmesdFMF4Ue8_m-83gAfkEkhH1Ly6iL4XCzHvY-AMweAguV2cFpM1rE4N6_0Mg",
     });
 
     if (currentToken) {
